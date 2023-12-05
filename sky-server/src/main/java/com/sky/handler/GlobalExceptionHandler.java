bfdc2b82da94cc76b1a52d40b7c5e2787fad0a09
liunian <321585854@qq.com>
2023-12-05 10:51:13 +0800

    苍穹外卖项目初始代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +26,20 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
-
+    /**
+     * 处理sql异常
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+       String message = ex.getMessage();
+       if(message.contains("Duplicate entry")){
+           String[] split = message.split(" ");
+           String username = split[2];
+           String msg = username + MessageConstant.ALREDY_EXISTS;
+           return Result.error(msg);
+       }
+       else {
+           return Result.error(MessageConstant.UNKNOWN_ERROR);
+       }
+    }
 }
